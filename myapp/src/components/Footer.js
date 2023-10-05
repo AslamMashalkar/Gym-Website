5a731a2c1bed434a6499382c81f811739ec364d8
kaif <kaif@gmail.com>
2023-10-05 12:42:41 +0530

    Added scrollToTop functionality

--- myapp/src/components/Footer.js
+++ myapp/src/components/Footer.js
@@ -1,61 +1,62 @@
-import { Link } from "react-router-dom"
-import Logo from '../images/logo.png'
-import { FaLinkedin } from "react-icons/fa"
-import { FaFacebook } from "react-icons/fa"
-import { AiOutlineTwitter } from "react-icons/ai"
-import { AiFillInstagram } from "react-icons/ai"
-
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Logo from '../images/logo.png';
+import { FaLinkedin } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { AiFillInstagram } from "react-icons/ai";
 
 const Footer = () => {
+  const navigate = useNavigate();
+
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
+
   return (
     <footer>
       <div className="container footer__container">
         <article>
           <Link to="/" className="logo">
-            <img src={Logo} alt="Footer Logo"/>
+            <img src={Logo} alt="Footer Logo" />
           </Link>
-          <p>lorem20 wkjdhc we have tried to make our website at its best by appliying varius properties</p>
+          <p>lorem20 wkjdhc we have tried to make our website at its best by applying various properties</p>
           <div className="footer__socials">
-            <a href="https://linkedin.com/" target="_blank" rel="norefferer noopener"><FaLinkedin/></a>
-
-            <a href="https://facebook.com/" target="_blank" rel="norefferer noopener"><FaFacebook/></a>
-
-            <a href="https://twitter.com/" target="_blank" rel="norefferer noopener"><AiOutlineTwitter/></a>
-
-            <a href="https://instagram.com/" target="_blank" rel="norefferer noopener"><AiFillInstagram/></a>
+            <a href="https://linkedin.com/" target="_blank" rel="noreferrer noopener"><FaLinkedin /></a>
+            <a href="https://facebook.com/" target="_blank" rel="noreferrer noopener"><FaFacebook /></a>
+            <a href="https://twitter.com/" target="_blank" rel="noreferrer noopener"><AiOutlineTwitter /></a>
+            <a href="https://instagram.com/" target="_blank" rel="noreferrer noopener"><AiFillInstagram /></a>
           </div>
         </article>
         <article>
           <h4>Permalinks</h4>
-          <Link to="/about">About</Link>
-          <Link to="/plans">Plans</Link>
-          <Link to="/trainers">Trainer</Link>
-          <Link to="/gallery">Gallery</Link>
-          <Link to="/contact">Contact</Link>
+          <Link to="/about" onClick={scrollToTop}>About</Link>
+          <Link to="/plans" onClick={scrollToTop}>Plans</Link>
+          <Link to="/trainers" onClick={scrollToTop}>Trainer</Link>
+          <Link to="/gallery" onClick={scrollToTop}>Gallery</Link>
+          <Link to="/contact" onClick={scrollToTop}>Contact</Link>
         </article>
 
         <article>
           <h4>Insights</h4>
-          <Link to="/s">Blog</Link>
-          <Link to="/s">Case Studies</Link>
-          <Link to="/s">Events</Link>
-          <Link to="/s">Communities</Link>
-          <Link to="/s">FAQs</Link>
+          <Link to="/s" onClick={scrollToTop}>Blog</Link>
+          <Link to="/s" onClick={scrollToTop}>Case Studies</Link>
+          <Link to="/s" onClick={scrollToTop}>Events</Link>
+          <Link to="/s" onClick={scrollToTop}>Communities</Link>
+          <Link to="/s" onClick={scrollToTop}>FAQs</Link>
         </article>
 
         <article>
           <h4>Get In Touch</h4>
-          <Link to="/contact">Contact Us</Link>
-          <Link to="/s">Support</Link>
+          <button onClick={() => { navigate('/contact'); scrollToTop(); }}>Contact Us</button>
+          <button onClick={() => { navigate('/s'); scrollToTop(); }}>Support</button>
         </article>
-
-        
       </div>
       <div className="footer__copyright">
         <small>2023 ASLAM MASHALKAR &copy; All Rights Reserved</small>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- myapp/src/components/Navbar.js
+++ myapp/src/components/Navbar.js
@@ -1,43 +1,43 @@
-import { useState } from 'react'
-import { Link, NavLink } from 'react-router-dom'
-import './navbar.css'
-import Logo from '../images/logo.png'
-import {links} from '../data'
-import {FaBars} from 'react-icons/fa'
-import {MdOutlineClose} from 'react-icons/md'
-
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import './navbar.css';
+import Logo from '../images/logo.png';
+import { links } from '../data';
+import { FaBars } from 'react-icons/fa';
+import { MdOutlineClose } from 'react-icons/md';
 
 const Navbar = () => {
-    const [isNavShowing, setIsNavShowing] = useState(false);
+  const [isNavShowing, setIsNavShowing] = useState(false);
+
+  // Function to scroll to the top of the page
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
 
   return (
     <nav>
       <div className='container nav__container'>
-        <Link to="/" className='logo' onClick={() => setIsNavShowing(false)} >
-          <img src={Logo}/>
+        <Link to="/" className='logo' onClick={() => { setIsNavShowing(false); scrollToTop(); }}>
+          <img src={Logo} alt="Logo" />
         </Link>
         <ul className={`nav__links ${isNavShowing ? 'show__nav' : 'hide__nav'}`}>
-          {
-            links.map(({name, path}, index) =>{
-              return(
-                <li key={index}>
-                  <NavLink to={path} onClick={() => setIsNavShowing (prev => !prev)}>{name}
-                  </NavLink>
-                </li>
-              )
-            })
-          }
+          {links.map(({ name, path }, index) => {
+            return (
+              <li key={index}>
+                <NavLink to={path} onClick={() => { setIsNavShowing(prev => !prev); scrollToTop(); }}>
+                  {name}
+                </NavLink>
+              </li>
+            );
+          })}
         </ul>
-        
-        <button className='nav__button' onClick={() => setIsNavShowing (prev => !prev)}>
-          {
-            isNavShowing ? <MdOutlineClose/> : <FaBars/>
-          }
-        </button>
 
+        <button className='nav__button' onClick={() => setIsNavShowing(prev => !prev)}>
+          {isNavShowing ? <MdOutlineClose /> : <FaBars />}
+        </button>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
